72e7963e33d52f0122e7794d73801036006b603b
Srivas S <me20b175@smail.iitm.ac.in>
2024-03-06 18:49:29 +0530

    Final_submission

--- codes_BDL/BDL2.py
+++ codes_BDL/BDL2.py
@@ -12,18 +12,18 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import ast
 base_url="https://www.ncei.noaa.gov/data/local-climatological-data/access/"  
-number_files=5
-year="2001"
+number_files=300 #Can change this
+year="2023" #Year too can be varied
 dag = DAG(
     'welcome_dag',
     default_args={'start_date': days_ago(1)},
-    schedule_interval='0 23 * * *',
+    schedule_interval=None,
     catchup=False
 )
 #get the html webpage
 #get_cmd=f"curl -o D:\\Workspace\\Climate_Dataset{year}.html {base_url}{year}"
-get_cmd=f"curl -L -o /opt/airflow/Climate_dataset{year}.html {base_url}{year}"
-saved_path=f"/opt/airflow/Climate_dataset{year}.html"
+get_cmd=f"curl -L -o /opt/airflow/Climate_dataset{year}.html {base_url}{year}" #curl command for getting webpage
+saved_path=f"/opt/airflow/Climate_dataset{year}.html" #place to be stored
 def choose_files(html_path,number_files):
     with open(html_path,'r') as file:
         data=file.read()
@@ -31,7 +31,7 @@ def choose_files(html_path,number_files):
         #get the links
     parse_file = BeautifulSoup(data, 'html.parser')    
     csv_links = [urljoin(base_url+year+'/',link.get('href')) for link in parse_file.find_all('a') if link.get('href').endswith('.csv')]
-    random_csv_links = random.sample(csv_links, number_files)
+    random_csv_links = random.sample(csv_links, number_files) #Gets all csv files from webpage and randomly chooses number_files amount of files
     print(random_csv_links)
     return random_csv_links
 def zip_files(csv_links,zip_filename):
@@ -42,7 +42,7 @@ def zip_files(csv_links,zip_filename):
         #print(i)
         #print("Downloading:", csv_links[i])
         res=requests.get(links)
-        if res.status_code==200:
+        if res.status_code==200: #If everything is okay
             filename=os.path.join(temp_dir, os.path.basename(links))
             with open(filename, 'wb') as csv_file:
                     csv_file.write(res.content)
@@ -55,17 +55,18 @@ def zip_files(csv_links,zip_filename):
         if os.path.isfile(file_path):
             os.remove(file_path)
     os.rmdir(temp_dir)
+#gif code using ffmpeg and Bash Operator
 #Task 1
-fetch_task=BashOperator(task_id="fetch_page",bash_command=get_cmd,dag=dag)
+fetch_task=BashOperator(task_id="fetch_page",bash_command=get_cmd,dag=dag) #executes the curl command
 #Task 2
 files_task=PythonOperator(task_id="choose_files",
                          python_callable=choose_files,
-                         op_kwargs={"html_path":saved_path,"number_files":number_files},
+                         op_kwargs={"html_path":saved_path,"number_files":number_files}, #Executes the choose_files function
                          dag=dag)
 #Task 3
 zip_task=PythonOperator(task_id="zip_files",
                         python_callable=zip_files,
-                        op_kwargs={"csv_links":"{{ ti.xcom_pull(task_ids='choose_files') }}",
+                        op_kwargs={"csv_links":"{{ ti.xcom_pull(task_ids='choose_files') }}", #This takes input from the choose files task by connecting with xcom
                                    "zip_filename":"/opt/airflow/Climate_dataset.zip"},
                                    dag=dag)
-fetch_task>>files_task>>zip_task
+fetch_task>>files_task>>zip_task #Final DAG created
--- codes_BDL/Beam.py
+++ codes_BDL/Beam.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 import random 
+import json
 import os
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
@@ -22,8 +23,8 @@ dag=DAG(
     schedule_interval=None,
     catchup=False
 )
-path_zip="/opt/airflow/Climate_dataset2001.zip"
-path_unzip="/opt/airflow/Climate_dataset2001"
+path_zip="/opt/airflow/Climate_dataset.zip" #Path to the zip file
+path_unzip="/opt/airflow/Climate_dataset2023" #Path to the unzipped folder
 #Task 1
 check_available=FileSensor(
     task_id="archive_available",
@@ -32,132 +33,145 @@ check_available=FileSensor(
     poke_interval=1,
     dag=dag,
 )
-stop_pipeline_task = BashOperator(
-    task_id='stop_pipeline',
-    bash_command='exit 1',
-    trigger_rule='one_failed', 
-    dag=dag,
-)
 #Task 2
 unzip_task=BashOperator(
     task_id='unzip_archive',
-    bash_command=f'if unzip -tq {path_zip}; then unzip -o {path_zip} -d {path_unzip}; else exit 1; fi',
+    bash_command=f'if unzip -tq {path_zip}; then unzip -o {path_zip} -d {path_unzip}; else exit 1; fi', #Unzip the file
     dag=dag,
     trigger_rule='all_success',
 )
-beam_options=PipelineOptions(
-    runner="DirectRunner",
-    project="my-project",
-    temp_location="/tmp",
-    direct_num_workers=10,
-    direct_running_mode='multi_processing'
-)
-def csv_df(folder_path,csv_file):
-    csv_path = os.path.join(folder_path, csv_file)
-    with open(csv_path, 'r') as file:
+def csv_df(csv_file):
+    with open(csv_file, 'r') as file:
         # Read CSV file and process it
         df=pd.read_csv(file)
     tuple_data=[]
+    col_anal=['HourlyDryBulbTemperature','HourlyWindSpeed']
+    all_analyse=['LATITUDE','LONGITUDE']+col_anal #Columns to be analysed
+    df[all_analyse]=df[all_analyse].apply(pd.to_numeric,errors='coerce') #Convert to numeric and coerce errors
+    df.dropna(how='any') #Drop all nan values
     for index,row in df.iterrows():
         lat=row['LATITUDE']
+        print(index)
         long=row['LONGITUDE']
-        date=row['DATE'].dt.month
+        date=pd.to_datetime(row['DATE']).month
         hourly_temp=row['HourlyDryBulbTemperature']
         hourly_speed=row['HourlyWindSpeed']
-        tuple_data.append((date,lat,long,hourly_temp,hourly_speed))
+        tuple_data.append((date,float(lat),float(long),float(hourly_temp),float(hourly_speed))) #Convert everthing into float and put in list
     return tuple_data
 def csv_to_dffilter(path_folder): # This function converts the csv file into the required tuple format
-    with beam.Pipeline(options=beam_options) as pipeline:
-        csv_files = [file for file in os.listdir(path_folder) if file.endswith('.csv')]
+    with beam.Pipeline(runner='DirectRunner') as pipeline:
+        csv_files = [os.path.join(path_folder,file) for file in os.listdir(path_folder) if file.endswith('.csv')]
         result = (
             pipeline 
             | beam.Create(csv_files)
-            | beam.Map(lambda file: csv_df(path_folder, file))
-            | beam.combiners.ToList()
+            | beam.Map(csv_df)
+            | beam.Map(monthly_average)
+            | beam.io.WriteToText('/opt/airflow/tuple_data.txt')
         )
-        return result
+text_address='/opt/airflow/tuple_data.txt-00000-of-00001'
 #Now let us create the function that will compute the monthly averages for the same latitude and longitude 
 def monthly_average(data_list):
-    data_df=pd.DataFrame(data_list,columns=['Month','Latitude','Longitude','Temperature','WindSpeed'])
-    result_df=data_df.groupby(['Month','Latitude','Longitude']).agg({'Temperature':'mean','WindSpeed':'mean'}).reset_index()
-    agg_data={}
-    for _,row in result_df.iterrows():
-        lat=row['Latitude']
-        long=row['Longitude']
-        month=row['Month']
-        temp=row['Temperature']
-        speed=row['WindSpeed']
-        if (lat,long) not in agg_data:
-            agg_data[(lat,long)]=[]
-        agg_data[(lat,long)].append((temp,speed))
-    final_result=[(lat,long,data) for (lat,long),data in agg_data.items()]
-    return final_result
-def beam_average(data_list):# Parallelizes the monthly average function
-    with beam.Pipeline(options=beam_options) as pipeline:
-        result = (
-            pipeline
-            | beam.Create(data_list)
-            | beam.Map(monthly_average)
-            | beam.combiners.ToList()
-        )
-        return result 
+    data_df = pd.DataFrame(data_list, columns=['Month', 'Latitude', 'Longitude', 'Temperature', 'WindSpeed'])
+    data_df.dropna(inplace=True)
+    data_df['Temperature']=(data_df['Temperature']-32)*5/9
+    final_result = []
+    result_df = data_df.groupby(['Month', 'Latitude', 'Longitude']).agg({'Temperature': 'mean', 'WindSpeed': 'mean'}).reset_index() #Use groupby to find average temperature and wind speed for each month
+    for _, row in result_df.iterrows():
+        lat = row['Latitude']
+        long = row['Longitude']
+        month = row['Month']
+        temp = row['Temperature']
+        speed = row['WindSpeed']
+        final_result.append([lat, long, month, temp, speed])
+    return json.dumps(final_result)
 #Now time to use geopandas and geo datasets
-def hmap(avg_list):
-    latitude = [lat for (lat, lon), (temp, speed) in avg_list]
-    longitude = [lon for (lat, lon), (temp, speed) in avg_list]
-    temperature = [temp for (lat, lon), (temp, speed) in avg_list]
-    wspeed = [wspeed for (lat, lon), (temp, wspeed) in avg_list]
+def hmap(link):
+    avg_list = []
+    with open(link, 'r') as file:
+        for line in file:
+            local_data = json.loads(line)
+            avg_list.extend(local_data)  # Extend instead of append to flatten the nested lists
+    
+    latitude = [lat for lat, lon, month, temp, wspeed in avg_list]#Get latitude longitude etc
+    longitude = [lon for lat, lon, month, temp, wspeed in avg_list]
+    month=[month for lat, lon, month, temp, wspeed in avg_list]
+    temperature = [temp for lat, lon, month, temp, wspeed in avg_list]
+    wspeed = [wspeed for lat, lon, month, temp, wspeed in avg_list]
     data_df = pd.DataFrame({
-    'Latitude': latitude,
-    'Longitude': longitude,
-    'AvgTemperature': temperature,
-    'AvgWSpeed': wspeed
+        'Latitude': latitude,
+        'Longitude': longitude,
+        'Month': month,
+        'AvgTemperature': temperature,
+        'AvgWSpeed': wspeed
     })
     gdf = gpd.GeoDataFrame(data_df, geometry=gpd.points_from_xy(data_df.Longitude, data_df.Latitude))
-    fig, ax = plt.subplots(figsize=(10, 10))
-    fig, ax = plt.subplots(figsize=(10, 10))
-    gdf.plot(ax=ax, column='AvgTemperature', cmap='coolwarm', markersize=50, alpha=0.8, legend=True)
-    ax.set_title('Temperature')
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
-    plt.grid(False)
-    plt.axis('equal')
-    plt.show()
-    fig, ax = plt.subplots(figsize=(10, 10))
-    gdf.plot(ax=ax, column='AvgWSpeed', cmap='viridis', markersize=50, alpha=0.8, legend=True)
-    ax.set_title('Speed Visualization')
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
-    plt.grid(False)
-    plt.axis('equal')
-    plt.show()
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    unique_months = gdf['Month'].unique()
+    temp_min = gdf['AvgTemperature'].min()
+    temp_max = gdf['AvgTemperature'].max()# This is done to fix scale in vmin and vmax
+    wind_speed_min = gdf['AvgWSpeed'].min()
+    wind_speed_max = gdf['AvgWSpeed'].max()
+    for month in unique_months:
+    # Filter the GeoDataFrame for the current month
+        gdf_month = gdf[gdf['Month'] == month]
+    
+    # Plot the world basemap
+    
+
+    # Plot Temperature for the current month
+        ax = world.plot( color='lightgrey', edgecolor='black',markersize=1,vmin=temp_min, vmax=temp_max)
+        gdf_month.plot(column='AvgTemperature', vmin=temp_min, vmax=temp_max,ax=ax, cmap='coolwarm', legend=True, 
+                   legend_kwds={'label': "AvgTemperature", 'orientation': "horizontal"})
+        ax.set_title(f'Temperature for Month {month}')
+        plt.axis('scaled')
+        plt.savefig(f'/opt/airflow/temperature/plot_month_{month}_temperature.png')
+
+    # Plot Wind Speed for the current month
+        ax = world.plot( color='lightgrey', edgecolor='black',markersize=1, vmin=wind_speed_min, vmax=wind_speed_max)
+        gdf_month.plot(column='AvgWSpeed',vmin=wind_speed_min, vmax=wind_speed_max, ax=ax, cmap='viridis', legend=True, 
+                   legend_kwds={'label': "AvgWSpeed", 'orientation': "horizontal"})
+        ax.set_title(f'Wind Speed for Month {month}')
+        plt.axis('scaled')
+        plt.savefig(f'/opt/airflow/wind/Wind_Speed_for_Month_{month}.png')
+    # Show the plots
+        plt.tight_layout()
+        plt.show()
 #Task 3
-tuple_gen=PythonOperator(
+#make the gif
+ #These are the files to be used to convert to GIF
+output_gif='/opt/airflow/temperature/temperature_302.gif'
+frame_rate = 2
+input_pattern = "/opt/airflow/temperature/plot_month_%d.0_temperature.png"
+gif_task_1= BashOperator(
+    task_id='create_gif_task',
+    bash_command='ffmpeg -framerate {} -i {} {}'.format(frame_rate, input_pattern, output_gif), #make gif for temperature
+    dag=dag,
+)
+input_pattern2="/opt/airflow/wind/Wind_Speed_for_Month_%d.0.png"
+output_gif2='/opt/airflow/wind/wind_302.gif'
+gif_task_2= BashOperator(
+    task_id='create_gif_task2',
+    bash_command='ffmpeg -framerate {} -i {} {}'.format(frame_rate, input_pattern2, output_gif2), #make gif for wind speed
+    dag=dag,
+)
+tupleavg_gen=PythonOperator(
     task_id="Listdf",
     python_callable=csv_to_dffilter,
-    op_kwargs={"path_folder":path_unzip},
+    op_kwargs={"path_folder":path_unzip}, #This executes the csv_to_dffilter function
     dag=dag,
 )
 #Task 4
-avg_gen=PythonOperator(
-    task_id="avg_tv",
-    python_callable=beam_average,
-    op_kwargs={"data_list":"{{ ti.xcom_pull(task_ids='Listdf') }}"},
-    dag=dag,
-)
 #Task 5
 hmap_gen=PythonOperator(
     task_id="plot_gen",
     python_callable=hmap,
-    op_kwargs={"avg_list":"{{ ti.xcom_pull(task_ids='avg_tv') }}"}
+    op_kwargs={"link":text_address}, #This gives the 12 plots for temperature and wind speed
 )
 #Task 6
 delete_csv=BashOperator(
     task_id="csv_folderdel",
-    bash_command=f'rm {path_unzip}',
+    bash_command=f'rm -r {path_unzip}', #This finally deletes the extrated csv files to prevent memory buildup 
     dag=dag,
     trigger_rule='all_success'
 )
-check_available>>stop_pipeline_task>>unzip_task>>tuple_gen>>avg_gen>>hmap_gen>>delete_csv
-        
-
+check_available>>unzip_task>>tupleavg_gen>>hmap_gen>>gif_task_1>>gif_task_2>>delete_csv #Final DAG created
